becf0a0eee5e0b7a112e079e5851418c22eb70ae
chickooooo <kshitijrajale@gmail.com>
2024-01-09 14:11:57 +0530

    implemented ProductCrudUsecase + test cases

--- src/features/product/tests/test_product_crud_usecase.py
+++ src/features/product/tests/test_product_crud_usecase.py
@@ -9,6 +9,22 @@
     -- with parameter product_data of type 'dict'
     -- with return type of 'Product'
 
+- ProductCrudUsecase has a get_product() method
+    -- with parameter query_data of type 'dict'
+    -- with return type of 'Product | None'
+
+- ProductCrudUsecase has a get_products() method
+    -- with parameter query_data of type 'dict'
+    -- with return type of 'list[Product]'
+
+- ProductCrudUsecase has a update_product() method
+    -- with parameter updated_product of type 'Product'
+    -- with return type of 'None'
+
+- ProductCrudUsecase has a delete_product() method
+    -- with parameter query_data of type 'dict'
+    -- with return type of 'None'
+
 - When create_product() method is called with incorrect product_data
   it should raise ValueError.
 - When create_product() method is called with correct product_data
@@ -18,6 +34,46 @@
 - create_product() method should return Product if create() method
   of 'sql_service' return None.
 - create_product() method should return correct Product.
+
+- When get_product() method is called with incorrect query_data
+  it should raise TypeError.
+- When get_product() method is called with correct query_data
+  it should call read_single() method of 'sql_service'.
+- get_product() method should raise SQLException if read_single() method
+  of 'sql_service' raises SQLException.
+- get_product() method should return None if read_single() method
+  of 'sql_service' returns None.
+- get_product() method should return the Product if read_single() method
+  of 'sql_service' returns a Product.
+
+- When get_products() method is called with incorrect query_data
+  it should raise TypeError.
+- When get_products() method is called with correct query_data
+  it should call read_multiple() method of 'sql_service'.
+- get_products() method should raise SQLException if read_multiple() method
+  of 'sql_service' raises SQLException.
+- get_products() method should return [] if read_multiple() method
+  of 'sql_service' returns [].
+- get_products() method should return the list[Product] if read_multiple()
+  method of 'sql_service' returns a list[Product].
+
+- When update_product() method is called with incorrect updated_product
+  it should raise TypeError.
+- When update_product() method is called with correct updated_product
+  it should call update() method of 'sql_service'.
+- update_product() method should raise SQLException if update() method
+  of 'sql_service' raises SQLException.
+- update_product() method should return None if update() method
+  of 'sql_service' returns None.
+
+- When delete_product() method is called with incorrect query_data
+  it should raise TypeError.
+- When delete_product() method is called with correct query_data
+  it should call delete() method of 'sql_service'.
+- delete_product() method should raise SQLException if delete() method
+  of 'sql_service' raises SQLException.
+- delete_product() method should return None if delete() method
+  of 'sql_service' returns None.
 """
 
 
@@ -94,6 +150,97 @@ def test_create_product_present():
     # verify method return type
     signature = inspect.signature(create_method)
     assert signature.return_annotation is Product
+
+
+def test_get_product_present():
+    """ProductCrudUsecase has a get_product() method with parameters:
+    query_data: dict
+    and return type of 'Product | None'.
+    """
+
+    # verify get product method
+    get_method = getattr(ProductCrudUsecase, "get_product", None)
+    assert get_method is not None
+
+    # verify product_data parameter
+    signature = inspect.signature(get_method)
+    assert "query_data" in signature.parameters
+
+    # verify product_data type
+    assert signature.parameters["query_data"].annotation is dict
+
+    # verify method return type
+    signature = inspect.signature(get_method)
+    assert str(signature.return_annotation).endswith("Product | None")
+
+
+def test_get_products_present():
+    """ProductCrudUsecase has a get_products() method with parameters:
+    query_data: dict
+    and return type of list[Product].
+    """
+
+    # verify get product method
+    get_method = getattr(ProductCrudUsecase, "get_products", None)
+    assert get_method is not None
+
+    # verify product_data parameter
+    signature = inspect.signature(get_method)
+    assert "query_data" in signature.parameters
+
+    # verify product_data type
+    assert signature.parameters["query_data"].annotation is dict
+
+    # verify method return type
+    signature = inspect.signature(get_method)
+    assert (
+        str(signature.return_annotation)
+        == "list[features.product.models.product.Product]"
+    )
+
+
+def test_update_product_present():
+    """ProductCrudUsecase has a update_product() method with parameters:
+    updated_product: Product
+    and return type of None.
+    """
+
+    # verify update product method
+    update_method = getattr(ProductCrudUsecase, "update_product", None)
+    assert update_method is not None
+
+    # verify updated_product parameter
+    signature = inspect.signature(update_method)
+    assert "updated_product" in signature.parameters
+
+    # verify product_data type
+    assert signature.parameters["updated_product"].annotation is Product
+
+    # verify method return type
+    signature = inspect.signature(update_method)
+    assert signature.return_annotation is None
+
+
+def test_delete_product_present():
+    """ProductCrudUsecase has a delete_product() method with parameters:
+    query_data: dict
+    and return type of None.
+    """
+
+    # verify delete product method
+    delete_method = getattr(ProductCrudUsecase, "delete_product", None)
+    assert delete_method is not None
+
+    # verify query_data parameter
+    signature = inspect.signature(delete_method)
+    assert "query_data" in signature.parameters
+
+    # verify product_data type
+    assert signature.parameters["query_data"].annotation is dict
+
+    # verify method return type
+    signature = inspect.signature(delete_method)
+    assert signature.return_annotation is None
 
 
 def test_create_product_incorrect_data():
@@ -211,3 +358,371 @@ def test_create_product_return_correct_product():
     assert result.id == 1
     assert result.name == "banana"
     assert result.price == 5.99
+
+
+# constant error message
+QUERY_DATA_VALID_DICT = "'query_data' should be a valid dict."
+
+
+def test_get_product_incorrect_data():
+    """When get_product() method is called with incorrect query_data
+    it should raise ValueError."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # verify TypeError raised
+    with pytest.raises(TypeError) as exc_info:
+        product_crud_usecase.get_product("get")  # type: ignore
+
+    # verify error message
+    assert QUERY_DATA_VALID_DICT in str(exc_info.value)
+
+
+def test_get_product_sql_service_read():
+    """When get_product() method is called with correct query_data
+    it should call read_single() method of 'sql_service'."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # call get_product with correct data
+    product_crud_usecase.get_product({"name": "banana"})
+
+    # verify read_single method called once
+    mock.read_single.assert_called_once_with({"name": "banana"})
+
+
+def test_get_product_sql_exception():
+    """get_product() method should raise SQLException if read_single() method
+    of 'sql_service' raises SQLException."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # create a dummy exception
+    dummy_exception = SQLException("Some database exception")
+
+    # raise exception when read_single method called
+    mock.read_single.side_effect = dummy_exception
+
+    # verify SQLException raised
+    with pytest.raises(SQLException) as exc_info:
+        # call get_product with correct data
+        product_crud_usecase.get_product({"name": "banana"})
+
+    # verify exception
+    assert exc_info.value == dummy_exception
+
+
+def test_get_product_return_none():
+    """get_product() method should return None if read_single() method
+    of 'sql_service' returns None."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # return None when read_single method called
+    mock.read_single.return_value = None
+
+    # call get_product with correct data
+    result = product_crud_usecase.get_product({"name": "banana"})
+
+    # verify result
+    assert result is None
+
+
+def test_get_product_return_product():
+    """get_product() method should return the Product if read_single() method
+    of 'sql_service' returns a Product."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # create product
+    product = Product(id=1, name="banana", price=4.99)
+
+    # return product when read_single method called
+    mock.read_single.return_value = product
+
+    # call get_product with correct data
+    result = product_crud_usecase.get_product({"name": "banana"})
+
+    # verify result
+    assert result is product
+    # verify product
+    assert product.id == 1
+    assert product.name == "banana"
+    assert product.price == 4.99
+
+
+def test_get_products_incorrect_data():
+    """When get_products() method is called with incorrect query_data
+    it should raise ValueError."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # verify TypeError raised
+    with pytest.raises(TypeError) as exc_info:
+        product_crud_usecase.get_products("get")  # type: ignore
+
+    # verify error message
+    assert QUERY_DATA_VALID_DICT in str(exc_info.value)
+
+
+def test_get_products_sql_service_read():
+    """When get_products() method is called with correct query_data
+    it should call read_multiple() method of 'sql_service'."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # call get_products with correct data
+    product_crud_usecase.get_products({"name": "banana"})
+
+    # verify read_multiple method called once
+    mock.read_multiple.assert_called_once_with({"name": "banana"})
+
+
+def test_get_products_sql_exception():
+    """get_products() method should raise SQLException if read_multiple()
+    method of 'sql_service' raises SQLException."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # create a dummy exception
+    dummy_exception = SQLException("Some database exception")
+
+    # raise exception when read_multiple method called
+    mock.read_multiple.side_effect = dummy_exception
+
+    # verify SQLException raised
+    with pytest.raises(SQLException) as exc_info:
+        # call get_products with correct data
+        product_crud_usecase.get_products({"name": "banana"})
+
+    # verify exception
+    assert exc_info.value == dummy_exception
+
+
+def test_get_products_return_empty():
+    """get_products() method should return None if read_multiple() method
+    of 'sql_service' returns None."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # return [] when read_multiple method called
+    mock.read_multiple.return_value = []
+
+    # call get_products with correct data
+    result = product_crud_usecase.get_products({"name": "banana"})
+
+    # verify result
+    assert result == []
+
+
+def test_get_products_return_products():
+    """get_products() method should return the Product if read_multiple()
+    method of 'sql_service' returns a Product."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # create products
+    product_1 = Product(id=1, name="banana", price=4.99)
+    product_2 = Product(id=2, name="apple", price=6.99)
+
+    # return product when read_multiple method called
+    mock.read_multiple.return_value = [product_1, product_2]
+
+    # call get_products with correct data
+    result = product_crud_usecase.get_products({"name": "banana"})
+
+    # verify result type
+    assert isinstance(result, list)
+    # verify result
+    assert result == [product_1, product_2]
+
+
+def test_update_product_incorrect_data():
+    """When update_product() method is called with incorrect updated_product
+    it should raise TypeError."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # verify TypeError raised
+    with pytest.raises(TypeError) as exc_info:
+        product_crud_usecase.update_product([])  # type: ignore
+
+    # verify error message
+    assert "'updated_product' should be a valid model." in str(exc_info.value)
+
+
+def test_update_product_sql_service_update():
+    """When update_product() method is called with correct updated_product
+    it should call update() method of 'sql_service'."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # create product
+    product = Product(id=1, name="banana", price=4.99)
+
+    # call update_product with correct data
+    product_crud_usecase.update_product(product)
+
+    # verify update method called once
+    mock.update.assert_called_once_with(product)
+
+
+def test_update_product_sql_exception():
+    """update_product() method should raise SQLException if update() method
+    of 'sql_service' raises SQLException."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # create a dummy exception
+    dummy_exception = SQLException("Some database exception")
+
+    # raise exception when update method called
+    mock.update.side_effect = dummy_exception
+
+    # create product
+    product = Product(id=1, name="banana", price=4.99)
+
+    # verify SQLException raised
+    with pytest.raises(SQLException) as exc_info:
+        # call update_product with correct data
+        product_crud_usecase.update_product(product)
+
+    # verify exception
+    assert exc_info.value == dummy_exception
+
+
+def test_update_product_return_none():
+    """update_product() method should return None if update() method
+    of 'sql_service' returns None."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # return None when update method called
+    mock.update.return_value = None
+
+    # create product
+    product = Product(id=1, name="banana", price=4.99)
+
+    # call update_product with correct data
+    result = product_crud_usecase.update_product(product)
+
+    # verify result
+    assert result is None
+
+
+def test_delete_product_incorrect_data():
+    """When delete_product() method is called with incorrect query_data
+    it should raise TypeError."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # verify TypeError raised
+    with pytest.raises(TypeError) as exc_info:
+        product_crud_usecase.delete_product(-99)  # type: ignore
+
+    # verify error message
+    assert QUERY_DATA_VALID_DICT in str(exc_info.value)
+
+
+def test_delete_product_sql_service_update():
+    """When delete_product() method is called with correct query_data
+    it should call delete() method of 'sql_service'."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # call delete_product with correct data
+    product_crud_usecase.delete_product({"name": "apple"})
+
+    # verify delete method called once
+    mock.delete.assert_called_once_with({"name": "apple"})
+
+
+def test_delete_product_sql_exception():
+    """delete_product() method should raise SQLException if delete() method
+    of 'sql_service' raises SQLException."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # create a dummy exception
+    dummy_exception = SQLException("Some database exception")
+
+    # raise exception when delete method called
+    mock.delete.side_effect = dummy_exception
+
+    # verify SQLException raised
+    with pytest.raises(SQLException) as exc_info:
+        # call delete_product with correct data
+        product_crud_usecase.delete_product({"name": "apple"})
+
+    # verify exception
+    assert exc_info.value == dummy_exception
+
+
+def test_delete_product_return_none():
+    """delete_product() method should return None if delete() method
+    of 'sql_service' returns None."""
+
+    # create mock sql service
+    mock = Mock(spec=SQLService)
+    # create product crud usecase
+    product_crud_usecase = ProductCrudUsecase(mock)
+
+    # return None when delete method called
+    mock.delete.return_value = None
+
+    # call delete_product with correct data
+    result = product_crud_usecase.delete_product({"name": "apple"})
+
+    # verify result
+    assert result is None
--- src/features/product/usecases/product_crud_usecase.py
+++ src/features/product/usecases/product_crud_usecase.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel
 from features.product.models.product import Product
 from core.services.sql_service.sql_service import SQLService
 
@@ -21,6 +22,7 @@ class ProductCrudUsecase:
 
         Raises:
             ValueError: If product_data is not valid.
+            SQLException: If error with database.
 
         Returns:
             Product: Created product.
@@ -32,3 +34,91 @@ class ProductCrudUsecase:
         self.__sql_service.create(product)
 
         return product
+
+    def get_product(self, query_data: dict) -> Product | None:
+        """Get a single product from database matching the query.
+
+        Args:
+            query_data (dict): Query in key-value format.
+
+        Raises:
+            TypeError: If query_data is invalid.
+            SQLException: If error with database.
+
+        Returns:
+            Product | None: First found product else None.
+        """
+
+        # verify query_data type
+        if not isinstance(query_data, dict):
+            # raise type error
+            raise TypeError("'query_data' should be a valid dict.")
+
+        # read & return from sql service
+        return self.__sql_service.read_single(query_data)
+
+    def get_products(self, query_data: dict) -> list[Product]:
+        """Get all the products from database matching the query.
+
+        Args:
+            query_data (dict): Query in key-value format.
+
+        Raises:
+            TypeError: If query_data is invalid.
+            SQLException: If error with database.
+
+        Returns:
+            list[Product]: List of found products else [].
+        """
+
+        # verify query_data type
+        if not isinstance(query_data, dict):
+            # raise type error
+            raise TypeError("'query_data' should be a valid dict.")
+
+        # read & return from sql service
+        return self.__sql_service.read_multiple(query_data)
+
+    def update_product(self, updated_product: Product) -> None:
+        """Update existing product in database. Will do nothing
+        if product is not found.
+
+        Args:
+            updated_product (Product): Updated product object.
+
+        Raises:
+            TypeError: If updated_product is not a valid model.
+            SQLException: If error with database.
+
+        Returns: None
+        """
+
+        # verify updated_product type
+        if not isinstance(updated_product, BaseModel):
+            # raise type error
+            raise TypeError("'updated_product' should be a valid model.")
+
+        # update & return from sql service
+        return self.__sql_service.update(updated_product)
+
+    def delete_product(self, query_data: dict) -> None:
+        """Delete product(s) from database matching the query.
+        Will do nothing if no product is found.
+
+        Args:
+            query_data (dict): Query in key-value format.
+
+        Raises:
+            TypeError: If query_data is invalid.
+            SQLException: If error with database.
+
+        Returns: None
+        """
+
+        # verify query_data type
+        if not isinstance(query_data, dict):
+            # raise type error
+            raise TypeError("'query_data' should be a valid dict.")
+
+        # delete & return from sql service
+        return self.__sql_service.delete(query_data)
